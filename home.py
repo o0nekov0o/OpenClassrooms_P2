1852abdafa2833fbd784110bfef4829bdc7ca1cb
o0nekov0o <33200665+o0nekov0o@users.noreply.github.com>
2022-06-01 07:18:34 +0200

    . Ajout du fichier home.py . Ajout de la docstring . __main__ au lieu de main ==> Récupération des livres depuis la page d'accueil ==> 975 livres récupérés sur les 1000 présents ==> 994 en tout en utilisation combinée avec main.py (erreur de récupération de la page 9, 1er livre bloquant ?) (erreur de récupération des 5 derniers livres dernière page) ==> Différents axes d'amélioration :  - paralléliser, afin que chaque processeur gère une catégorie - ne pas réécrire les lignes déjà récupérées dans le CSV - écrire les données dans une liste plutôt que ligne par ligne (ajouter ensuite cette liste à l'intérieur du fichier CSV)
    . Ajout du fichier home.py
    . Ajout de la docstring
    . __main__ au lieu de main
    ==> Récupération des livres depuis la page d'accueil
    ==> 975 livres récupérés sur les 1000 présents
    ==> 994 en tout en utilisation combinée avec main.py
    (erreur de récupération de la page 9, 1er livre bloquant ?)
    (erreur de récupération des 5 derniers livres dernière page)
    ==> Différents axes d'amélioration : 
    - paralléliser, afin que chaque processeur gère une catégorie
    - ne pas réécrire les lignes déjà récupérées dans le CSV
    - écrire les données dans une liste plutôt que ligne par ligne
    (ajouter ensuite cette liste à l'intérieur du fichier CSV)

--- home.py
+++ home.py
@@ -1,45 +1,36 @@
-import requests
-from bs4 import BeautifulSoup
-import time
+import category_2
 
-import extract_book
-import load_book
+web_url = 'http://books.toscrape.com/index.html'
 
-index_url = 'https://books.toscrape.com/catalogue/category/books_1/index.html'  # global var
-
-def save_one_category(index_url):
+def save_pages_part1(web_url):
     """
-    Save all books of the home page (it handles pagination)
+    Save all books from the homepage (it handles pagination)
     """
-    suivant = 1  # permettra de relancer la boucle
-    url = index_url  # preserve global var
-    while suivant == 1:  # condition relance boucle
-        response = requests.get(url)  # 200 si ok
-        print(response)  # afficher code retour
-        if response.ok:  # si justement ok
-            soup = BeautifulSoup(response.text, features='html.parser')  # format txt, delete warnings
-            try:
-                for i in soup.find_all('div', {'class': 'image_container'}):  # plusieurs classes image_container
-                    lien = i.find('a')['href'].replace('../..', 'http://books.toscrape.com/catalogue')  # recup lien
-                    book_data = extract_book.get_book_data(lien)  # appel fonction get_book_data, retour dans book_data
-                    load_book.save_book_csv(book_data)  # appel foncion save_book_csv, enregistrer valeurs retournées
-                    time.sleep(0.00)  # meilleure lecture dans interpreter, si corrections bugs
-                if soup.find('li', {'class': 'next'}):  # si presence bouton suivant
-                    base_url = url[:url.rfind('/')]  # sup derniere partie url (initialement index)
-                    suffix_url = soup.find('li', {'class': 'next'}).find('a')['href']  # isole lien bouton suivant
-                    url = f"{base_url}/{suffix_url}"  # ajout lien bouton suivant au reste
-                    # url = f"{url[:url.rfind('/')]}/{soup.find('li', {'class': 'next'}).find('a')['href']}"
-                    suivant = 1  # relancer la boucle (car bouton suivant present)
-                else:
-                    suivant = 0  # sortir de la boucle (car bouton suivant absent)
-            except AttributeError:  # si description absente
-                continue
+    web_url = 'http://books.toscrape.com/catalogue/category/books_1/page-1.html'
+    for nb in range(1):  # afin de mettre try dans une boucle
+        try:
+            category_2.save_one_category(web_url)  # même variable pour appeler fonction
+        except AttributeError:  # si description absente
+            continue
 
-save_one_category(index_url)
+def save_pages_part2(web_url):
+    """
+    Save all books from the homepage (it handles pagination)
+    """
+    web_url = 'http://books.toscrape.com/catalogue/category/books_1/page-10.html'
+    for nb in range(1):  # afin de mettre try dans une boucle
+        try:
+            category_2.save_one_category(web_url)  # même variable pour appeler fonction
+        except AttributeError:  # si description absente
+            continue
+            
+save_pages_part1(web_url)
+save_pages_part2(web_url)
 
-if __name__ == "main":
+if __name__ == "__main__":
     book_data = get_book_data(EXAMPLE_URL)
     save_book_csv(book_data)
     save_one_category(index_url)
-    save_all_categories(site_url)
+    save_pages_part1(web_url)
+    save_pages_part2(web_url)
     save_image_file(url_image)
